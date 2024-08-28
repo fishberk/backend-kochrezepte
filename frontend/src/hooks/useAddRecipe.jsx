4b95b16f44df40e78fa0a5a22f8c757e00e0e1ef
Swift-TuttleA3 <r.panek888@gmail.com>
2024-08-28 12:24:10 +0200

    updated hooks

--- frontend/src/hooks/useAddRecipe.jsx
+++ frontend/src/hooks/useAddRecipe.jsx
@@ -17,3 +17,42 @@ const useAddRecipe = (recipes, setRecipes) => {
 };
 
 export default useAddRecipe;
+
+/* VARIANTE MIT FETCH
+import { useState } from 'react';
+
+const useAddRecipe = (recipes, setRecipes) => {
+  const [apiError, setApiError] = useState('');
+
+  const handleAddRecipe = async (newRecipe) => {
+    try {
+      const response = await fetch('http://localhost:5000/api/recipes', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newRecipe),
+      });
+
+      if (!response.ok) {
+        throw new Error('Fehler beim Hinzufügen des Rezepts.');
+      }
+
+      const addedRecipe = await response.json();
+      setRecipes([...recipes, addedRecipe]);
+    } catch (error) {
+      setApiError(error.message);
+    }
+  };
+
+  return { handleAddRecipe, apiError };
+};
+
+export default useAddRecipe;
+
+HAUPTUNTERSCHIED ZU VARIANTE MIT AXIOS IST DIE VERWENDUNG VON FETCH ANSTELLE VON AXIOS
+
+
+
+*/
+
