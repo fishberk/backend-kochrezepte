2e68a0dab62051a32d2cb8b93ce39a894a596356
fishberk <150148990+fishberk@users.noreply.github.com>
2024-08-28 12:42:40 +0200

    added recipe model

---
+++ backend/src/models/recipe.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const recipeSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  ingredients: [
+    {
+      name: { type: String, required: true },
+      quantity: { type: String, required: true },
+    },
+  ],
+  instructions: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  dateAdded: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
